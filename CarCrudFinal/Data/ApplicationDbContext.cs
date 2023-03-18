fdfc1555f358bf668a90cfc5e0ce28bbea0f6732
user.email <sam777manss@gmail.com>
2023-03-18 14:27:10 +0530

    first

---
+++ CarCrudFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CarCrudFinal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarCrudFinal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cars> Cars { get; set; }
+    }
+}
